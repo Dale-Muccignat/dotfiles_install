497a0b6c1372b24f1265af2d2190cba0b111ce6f
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-05-17 19:43:21 +1000

    update packages

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:Dale-Muccignat/dotfiles $HOME/.dotfiles
 dotfiles checkout
-sudo pacman -S zsh sshfs firefox picom nitrogen emacs fzf xorg-xset rofi ranger gnome texlive-most
-yay -S ttf-nerd-fonts-symbols oh-my-zsh-git auto-cpufreq
+sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim
+yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome
 chsh --shell /bin/zsh
 nitrogen /path/to/wallpaper
 sudo systemctl enable --now auto-cpufreq.service
