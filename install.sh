7eae4142e29d5b07b4c7187e4622a34844fbb582
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-05-17 20:22:12 +1000

    remove nitrogen

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:Dale-Muccignat/dotfiles $HOME/.dotfiles
 dotfiles checkout
 sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim
-yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome
-chsh --shell /bin/zsh
-nitrogen /path/to/wallpaper
+yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug
+#chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
 git config --global core.excludesfile ~/.gitignore
