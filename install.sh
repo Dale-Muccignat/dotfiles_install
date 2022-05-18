36d47c7671712cb99c4ba1489b59f1f2f0281937
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-05-18 11:38:11 +1000

    edits

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git clone --bare git@github.com:Dale-Muccignat/dotfiles $HOME/.dotfiles
-dotfiles checkout
+rm ~/.config/i3/config
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
 sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim
 yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug
-#chsh --shell /bin/zsh
+chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
 git config --global core.excludesfile ~/.gitignore
+git config --global user.email "dale.muccignat@my.jcu.edu.au"
+git config --global user.name "Dale Muccignat"
