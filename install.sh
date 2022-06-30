88eb4f11b591a7ffa3f5aef1ef2c6ca10fa4f007
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-06-30 17:42:43 +1000

    slack

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone --bare git@github.com:Dale-Muccignat/dotfiles $HOME/.dotfiles
 rm ~/.config/i3/config
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
 sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot
-yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug
+yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack
 chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
 git config --global core.excludesfile ~/.gitignore
