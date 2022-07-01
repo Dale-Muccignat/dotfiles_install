ec57f607624346eedbb9168e6b45dfa8909f7076
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-07-01 16:14:02 +1000

    i3lock improvement

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone --bare https://github.com/Dale-Muccignat/dotfiles $HOME/.dotfiles
 rm ~/.config/i3/config
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
 sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot polybar
-yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack
+yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack i3lock-fancy-rapid-git
 chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
 git config --global core.excludesfile ~/.gitignore
