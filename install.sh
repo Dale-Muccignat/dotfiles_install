78559489cc15c1c4d8537981b30095ee8f4d3934
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-07-01 11:30:47 +1000

    polybar

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone --bare https://github.com/Dale-Muccignat/dotfiles $HOME/.dotfiles
 rm ~/.config/i3/config
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
-sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot
+sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot polybar
 yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack
 chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
