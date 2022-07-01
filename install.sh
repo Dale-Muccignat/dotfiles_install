bb605cf38a280ff5c76cec82542cc0fce11c7794
Dale-Muccignat <dale.muccignat@my.jcu.edu.au>
2022-07-01 11:00:44 +1000

    added ssh-keygen

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@github.com:Dale-Muccignat/dotfiles $HOME/.dotfiles
+git clone --bare https://github.com/Dale-Muccignat/dotfiles $HOME/.dotfiles
 rm ~/.config/i3/config
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
 sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot
@@ -9,3 +9,5 @@ git config --global core.excludesfile ~/.gitignore
 git config --global user.email "dale.muccignat@my.jcu.edu.au"
 git config --global user.name "Dale Muccignat"
 xdg-mime default org.gnome.Evince.desktop application/pdf
+ssh-keygen -t ed25519
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url git@github.com:Dale-Muccignat/dotfiles
