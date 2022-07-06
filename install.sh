f908e4ddd8da121254fbb07b9df98c73a324ab97
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2022-07-06 20:12:50 +1000

    added autostart

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 git clone --bare https://github.com/Dale-Muccignat/dotfiles $HOME/.dotfiles
 rm ~/.config/i3/config
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout desktop
-sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot polybar
-yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack i3lock-fancy-rapid-git
+sudo pacman -S zsh sshfs picom nitrogen fzf xorg-xset rofi ranger gnome gvim evince playerctl dunst flameshot polybar dex
+yay -S nerd-fonts-complete oh-my-zsh-git auto-cpufreq google-chrome vim-plug slack i3lock-fancy-rapid-git autotiling
 chsh --shell /bin/zsh
 sudo systemctl enable --now auto-cpufreq.service
 git config --global core.excludesfile ~/.gitignore
